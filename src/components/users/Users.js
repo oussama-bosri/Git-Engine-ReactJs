c419ef6f90f4bee44e4b0c4e93f54068c1d118cb
OUSSAMA <58121982+OUSSAMABOSRI@users.noreply.github.com>
2021-06-30 14:22:55 +0100

    first commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Users: [{}],
+    };
+  }
+
+  render() {
+      return <div>
+        
+    </div>;
+  }
+}
+
+export default Users;
